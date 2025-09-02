6974a833ea542cb79af5e141f823a339aaa04736
abhishek2001panwar <abhishek.panwar.cseiot.2022@miet.ac.in>
2025-09-02 12:04:52 +0530

    cloudinary setuo

--- src/app/api/schools/route.ts
+++ src/app/api/schools/route.ts
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import { prisma } from "../../../lib/prisma";
 import cloudinary from "../../../lib/cloudinary";
 
-export const dynamic = "force-dynamic"; // For Vercel/Render
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function POST(req: Request) {
   try {
